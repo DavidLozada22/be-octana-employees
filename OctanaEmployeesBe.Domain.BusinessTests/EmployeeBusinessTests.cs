856b4bab859efbb2af1e46bb06e6c7bbd5566b8a
David Lozada <davidandreslozada@gmail.com>
2023-10-03 14:30:14 -0500

    Third commit

--- OctanaEmployeesBe.Domain.BusinessTests/EmployeeBusinessTests.cs
+++ OctanaEmployeesBe.Domain.BusinessTests/EmployeeBusinessTests.cs
@@ -1,16 +1,8 @@
-﻿using be_octana_employees.Controllers;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using OctanaEmployeesBe.Domain.Business;
 using OctanaEmployeesBe.Domain.Contracts;
 using OctanaEmployeesBe.Domain.Entities;
 using OctanaEmployeesBe.Infrastructure.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OctanaEmployeesBe.Domain.Business.Tests
 {
@@ -19,19 +11,36 @@ namespace OctanaEmployeesBe.Domain.Business.Tests
     {
         private IEmployeeBusiness<Employee> _employeeBusiness;
 
-        public EmployeeBusinessTests(IEmployeeBusiness<Employee> employeeBusiness)
+        [TestInitialize]
+        public void Initialize()
         {
-            _employeeBusiness = employeeBusiness;
+            //var mockEmployeeRepository = new Mock<IEmployeeRepository<Employee>>().Object;
+            //_employeeBusiness = new EmployeeBusiness(mockEmployeeRepository);
+
+            var mockEmployeeRepository = new Mock<IEmployeeRepository<Employee>>();
+
+            mockEmployeeRepository.Setup(repo => repo.GetEmployeeByIdsAsync(It.IsAny<int>()))
+                .ReturnsAsync((int id) => new Employee
+                {
+                    id = id,
+                    employee_name = "Tiger Nixon",
+                    employee_salary = 320800,
+                    employee_age = 61,
+                    profile_image = "",
+                    employee_annual_salary = 320800 * 12
+                });
+
+            // Convierte el mock en un objeto real
+            var employeeRepository = mockEmployeeRepository.Object;
+
+            // Crea la instancia de EmployeeBusiness utilizando el objeto mock
+            _employeeBusiness = new EmployeeBusiness(employeeRepository);
         }
 
         [TestMethod()]
         public async Task GetEmployeeByIdsAsyncTest()
         {
             // Arrange
-            var mockEmployeeRepository = new Mock<IEmployeeRepository<Employee>>().Object;
-
-            _employeeBusiness = new EmployeeBusiness(mockEmployeeRepository);
-            
             var expectedEmployee = new Employee { 
                 id = 1, 
                 employee_name = "Tiger Nixon", 
@@ -45,7 +54,12 @@ namespace OctanaEmployeesBe.Domain.Business.Tests
             var result = await _employeeBusiness.GetEmployeeByIdsAsync(1);
 
             //Assert
-            Assert.AreEqual(expectedEmployee, result);
+            Assert.AreEqual(expectedEmployee.id, result.id);
+            Assert.AreEqual(expectedEmployee.employee_name, result.employee_name);
+            Assert.AreEqual(expectedEmployee.employee_salary, result.employee_salary);
+            Assert.AreEqual(expectedEmployee.employee_age, result.employee_age);
+            Assert.AreEqual(expectedEmployee.profile_image, result.profile_image);
+            Assert.AreEqual(expectedEmployee.employee_annual_salary, result.employee_annual_salary);
         }
     }
 }
